87b607e293cb489461f459fe1d3d6d31c52156dc
Noah Seavers <nbseavers@gmail.com>
2024-06-25 15:04:39 -0500

    setup turbo frames for editing quotes

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,36 +2,28 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first) # Referenve to the first fixture quote
+    @quote = quotes(:first)
   end
 
-  test "Creating a new quote" do 
-    # When we visit the Quotes#index page
-    # we expect to see a title with the text "Quotes
-    visit quotes_path
-    assert_selector "h1", text: "Quotes"
-
-    # When we click the link with the text "New Quote"
-    # we expect to land on a page with the title "New quote"
-    click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
-    # When we fill in the name input with "Capybara quote"
-    # and we click "Create Quote"
-    fill_in "Name", with: "Capybara quote"
-    click_on "Create quote"
-
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list.
-    assert_selector "h1", text: "Quotes"
-    assert_text "Capybara quote"
-  end
-  
-  test "Showing a quote" do 
+  test "Showing a quote" do
     visit quotes_path
     click_link @quote.name
 
-  assert_selector "h1", text: @quote.name
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "Creating a new quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "New quote"
+    fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
+    click_on "Create quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
   end
 
   test "Updating a quote" do
@@ -39,16 +31,16 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
     assert_text "Updated quote"
   end
 
-  test "Destroying a quote" do 
+  test "Destroying a quote" do
     visit quotes_path
     assert_text @quote.name
 
